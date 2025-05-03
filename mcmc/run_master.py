ca03085e57d6a8f03b6e6d065c2827bc1447e5c1
GPT-Design <gpt.design500@gmail.com>
2025-05-03 21:44:03 +0930

    Update Cobaya driver priors to uniform list-shorthand

--- mcmc/run_master.py
+++ mcmc/run_master.py
@@ -3,21 +3,26 @@ from cobaya.run import run
 
 info = yaml_load("""
 likelihood:
-  bao.desi_2024_bao_all:           null
-  planck_2018_lowl.TT:             null
-  planck_2018_lowl.EE:             null
-  planck_2018_highl_CamSpec.TTTEEE: null
+  bao.desi_2024_bao_all:             null
+  planck_2018_lowl.TT:               null
+  planck_2018_lowl.EE:               null
+  planck_2018_highl_CamSpec.TTTEEE:  null
 
 params:
-  alpha:          [0., 0.2]
-  log10S0_over_H02: [-2, 2]
-  omega_b:        0.0224 ± 0.0001
-  omega_cdm:      0.12   ± 0.002
-  h:              0.68   ± 0.01
-  n_s:            0.965  ± 0.004
+  # 3T_E freedom
+  alpha:           [0.0, 0.2]
+  log10S0_over_H02: [-2.0, 2.0]
+
+  # “Planck-lite” uniform priors
+  omega_b:   [0.0223, 0.0225]
+  omega_cdm: [0.118 , 0.122 ]
+  h:         [0.67  , 0.69  ]
+  n_s:       [0.96  , 0.97  ]
+
 sampler:
   mcmc:
     Rminus1_stop: 0.01
+    max_tries:    900000
 """)
 
 run(info, resume=False)
