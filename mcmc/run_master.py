c5666169dced60e4df26cce067fcf9a2dccc6c25
GPT-Design <gpt.design500@gmail.com>
2025-05-04 19:12:07 +0930

    Run high-ℓ + DESI chain; planck-lite priors, skip low-ℓ for now

--- mcmc/run_master.py
+++ mcmc/run_master.py
@@ -4,8 +4,8 @@ from cobaya.run import run
 info = yaml_load("""
 likelihood:
   bao.desi_2024_bao_all:             null
-  planck_2018_lowl.TT:               null
-  planck_2018_lowl.EE:               null
+  #planck_2018_lowl.TT:               null
+  #planck_2018_lowl.EE:               null
   planck_2018_highl_CamSpec.TTTEEE:  null
 
 params:
@@ -14,10 +14,10 @@ params:
   log10S0_over_H02: [-2.0, 2.0]
 
   # “Planck-lite” uniform priors
-  omega_b:   [0.0223, 0.0225]
-  omega_cdm: [0.118 , 0.122 ]
-  h:         [0.67  , 0.69  ]
-  n_s:       [0.96  , 0.97  ]
+  omega_b:   [0.0222, 0.0226]    # ±0.0002
+  omega_cdm: [0.118,  0.122 ]    # ±0.002
+  h:         [0.67,   0.69  ]    # ±0.01
+  n_s:       [0.964,  0.968 ]    # ±0.002
 
 sampler:
   mcmc:
